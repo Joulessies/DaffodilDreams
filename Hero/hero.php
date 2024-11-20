5acde0f416c6b0ee9bd0cbed989e4167629784ad
Julius San Jose <112174347+Joulessies@users.noreply.github.com>
2024-11-20 13:51:44 +0800

    Intial Commit

---
+++ Hero/hero.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Hero/hero.css">
+</head>
+
+<body>
+    <section class="hero">
+        <div class="images-counter">
+            <img src="images/Tulips/tulip1.png" class="rounded float-start" alt="tulip">
+            <img src="images/Roses/Rose1.png" class="rounded float-start" alt="rose">
+            <img src="images/Sunflowers/Sunflower1.png" class="rounded float-start" alt="sunflower">
+            <img src="images/FlowerBouquet/Flowerbouquet1.png" class="rounded float-start" alt="bouquets">
+            <img src="images/Carnations/Carnation1.png" class="rounded float-start" alt="carnation">
+        </div>
+    </section>
+
+
+</body>
+
+</html>
